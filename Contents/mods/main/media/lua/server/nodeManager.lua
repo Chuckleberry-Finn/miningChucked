fff62dc0ce7095137805cde9c09a9ba05b8410e6
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-06-07 20:55:44 -0400

    Update nodeManager.lua

--- Contents/mods/main/media/lua/server/nodeManager.lua
+++ Contents/mods/main/media/lua/server/nodeManager.lua
@@ -2,9 +2,16 @@ local nodeManager = {}
 
 nodeManager.zones = {}
 
+
 ---nodeZone template
-nodeManager.nodeZone = { x1=-1, y1=-1, x2=-1, y2=-1, maxNodes=0, respawnTimer=0, minerals = {}, }
 -- minerals = {}:  `["mineralID"]=chance` to appear
+nodeManager.nodeZone = {
+    x1=-1, y1=-1, x2=-1, y2=-1,
+    maxNodes=0, respawnTimer=0,
+    minerals = {},
+    currentNodes = { {x=0, y=0,}, },--mineral="", hits=0},
+}
+
 
 function nodeManager.addZone(x1, y1, x2, y2, minerals)
     local newZone = copyTable(nodeManager.nodeZone)
@@ -15,6 +22,19 @@ end
 function nodeManager.init(isNewGame)
     print("nodeManager:init")
     nodeManager.zones = ModData.getOrCreate("miningChucked_zones")
+end
+
+
+function nodeManager.spawnNode(nodeZone)
+    --spawnNode
+    local x, y = 0, 0
+
+    --pick empty spot in zone, using currentNodes to get random
+
+
+    local node --= set modData to nodeZone to match against
+
+    table.insert(nodeZone.currentNodes, {x, y} )
 end
 
 
