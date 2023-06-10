ded5298990a1e6c64ded29ce3c93fdebb5563fa4
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-06-09 20:50:01 -0400

    skill books

---
+++ Contents/mods/main/media/lua/server/miningSkillBooks.lua
@@ -0,0 +1,9 @@
+require 'XpSystem/XPSystem_SkillBook'
+
+SkillBook["Mining"] = {}
+SkillBook["Mining"].perk = Perks.Mining
+SkillBook["Mining"].maxMultiplier1 = 3
+SkillBook["Mining"].maxMultiplier2 = 5
+SkillBook["Mining"].maxMultiplier3 = 8
+SkillBook["Mining"].maxMultiplier4 = 12
+SkillBook["Mining"].maxMultiplier5 = 16
