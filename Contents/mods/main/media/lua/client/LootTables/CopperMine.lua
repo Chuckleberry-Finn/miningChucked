0bea400b57e809028939dfc659a87ff601c96345
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-06-15 20:58:23 -0400

    updated mines format

--- Contents/mods/main/media/lua/client/LootTables/CopperMine.lua
+++ Contents/mods/main/media/lua/client/LootTables/CopperMine.lua
@@ -17,34 +17,10 @@ miningMod.resources["Copper Mine"] = {
             requireLevel = true,
             fixed = false,
             amountPerLevel = {
-                {
-                    levels = { 0, 1 },
-                    amounts = {
-                        min = 0,
-                        max = 1
-                    }
-                },
-                {
-                    levels = { 2, 3 },
-                    amounts = {
-                        min = 1,
-                        max = 3
-                    }
-                },
-                {
-                    levels = { 4, 5 },
-                    amounts = {
-                        min = 2,
-                        max = 4
-                    }
-                },
-                {
-                    levels = { 6, 7, 8, 9, 10 },
-                    amounts = {
-                        min = 3,
-                        max = 6
-                    }
-                }
+                { levels = { 0, 1 }, amounts = { min = 0, max = 1 } },
+                { levels = { 2, 3 }, amounts = { min = 1, max = 3 } },
+                { levels = { 4, 5 }, amounts = { min = 2, max = 4 } },
+                { levels = { 6, 7, 8, 9, 10 }, amounts = { min = 3, max = 6 } },
             }
         },
     }
